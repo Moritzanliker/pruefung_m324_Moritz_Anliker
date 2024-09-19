c26a3cc0ea4b68a21a2ed173361147c0804c8e9e
Moritz Anliker <moritz.anliker@gmail.com>
2024-09-19 10:57:09 +0200

    linter fixed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import chalk from 'chalk'
 
-let myUnusedVariable = 10;  // Trigger 'no-unused-vars' error
+const myUnusedVariable = 10;  // Trigger 'no-unused-vars' error
 
-if (myUnusedVariable == 10) {  // Trigger 'eqeqeq' error
+if (myUnusedVariable === 10) {  // Trigger 'eqeqeq' error
     console.log(chalk.bgBlue("it works"))
 }
